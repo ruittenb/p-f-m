f051e4b9e9ecb4a75bec0d30388759c22bada338
ruittenb <ruittenb@users.noreply.github.com>
2011-04-03 18:07:28 +0000

    Version 1.94.5
     * added '!' symbol to SVN column heading when a status command is running
     * minor fixes

--- pfm.pl
+++ pfm.pl
@@ -1,13 +1,13 @@
 #!/usr/bin/env perl
 #
 ##########################################################################
-# @(#) pfm.pl 2009-10-23 v1.94.4b
+# @(#) pfm.pl 2009-10-28 v1.94.5
 #
 # Name:			pfm
-# Version:		1.94.4b
+# Version:		1.94.5
 # Author:		Rene Uittenbogaard
 # Created:		1999-03-14
-# Date:			2009-10-23
+# Date:			2009-10-28
 # Usage:		pfm [ <directory> ] [ -s, --swap <directory> ]
 #				pfm { -v, --version | -h, --help }
 # Requires:		Term::ReadLine::Gnu (preferably)
@@ -6027,7 +6027,7 @@ up if you resize your terminal window to a smaller size.
 
 =head1 VERSION
 
-This manual pertains to C<pfm> version 1.94.4b.
+This manual pertains to C<pfm> version 1.94.5.
 
 =head1 AUTHOR and COPYRIGHT
 
