dd0fba161d991a9462c437313d88d9dec60176ae
ruittenb <ruittenb@22fe62ee-fe65-43d0-9c8d-b03beee93304>
2011-04-10 13:12:06 +0000

    Version 2.02.5-WIP. Moved files to lib/App/PFM.

---
+++ trunk/lib/App/PFM/History.pm
@@ -0,0 +1,304 @@
+#!/usr/bin/env perl
+#
+##########################################################################
+# @(#) PFM::History 0.02
+#
+# Name:			PFM::History.pm
+# Version:		0.02
+# Author:		Rene Uittenbogaard
+# Created:		1999-03-14
+# Date:			2010-04-01
+#
+
+##########################################################################
+
+=pod
+
+=head1 NAME
+
+PFM::History
+
+=head1 DESCRIPTION
+
+PFM History class. Reads and writes history files, holds the histories
+in memory, and coordinates how Term::ReadLine handles them.
+
+=head1 METHODS
+
+=over
+
+=cut
+
+##########################################################################
+# declarations
+
+package PFM::History;
+
+use base qw(PFM::Abstract Exporter);
+
+use Term::ReadLine;
+
+use strict;
+
+use constant {
+	H_COMMAND	=> 'history_command',
+	H_MODE		=> 'history_mode',
+	H_PATH		=> 'history_path',
+	H_REGEX		=> 'history_regex',
+	H_TIME		=> 'history_time',
+	H_PERLCMD	=> 'history_perlcmd',
+};
+
+our @EXPORT = qw(H_COMMAND H_MODE H_PATH H_REGEX H_TIME H_PERLCMD);
+
+my ($_pfm, $_keyboard,
+	@_command_history,
+	@_mode_history,
+	@_path_history,
+	@_regex_history,
+	@_time_history,
+	@_perlcmd_history,
+);
+
+my %HISTORIES = (
+	history_command	=> \@_command_history,
+	history_mode	=> \@_mode_history,
+	history_path	=> \@_path_history,
+	history_regex	=> \@_regex_history,
+	history_time	=> \@_time_history,
+	history_perlcmd	=> \@_perlcmd_history,
+);
+
+my $MAXHISTSIZE	= 70;
+my $CWDFILENAME	= 'cwd';
+my $SWDFILENAME	= 'swd';
+
+##########################################################################
+# private subs
+
+=item _init()
+
+Initializes this instance by instantiating a Term::ReadLine object.
+Called from the constructor.
+
+=cut
+
+sub _init {
+	my ($self, $pfm) = @_;
+	$_pfm      = $pfm;
+	$_keyboard = new Term::ReadLine('pfm');
+}
+
+=item _set_term_history()
+
+Uses the history list to initialise keyboard history in
+Term::ReadLine. This fails silently if our current
+implementation of Term::ReadLine doesn't support the setHistory()
+method.
+
+=cut
+
+sub _set_term_history {
+	my $self = shift;
+	if ($_keyboard->Features->{setHistory}) {
+		$_keyboard->SetHistory(@_);
+	}
+	return $_keyboard;
+}
+
+##########################################################################
+# constructor, getters and setters
+
+=item keyboard()
+
+Getter for the Term::ReadLine object.
+
+=cut
+
+sub keyboard {
+	return $_keyboard;
+}
+
+##########################################################################
+# public subs
+
+=item read()
+
+Reads the histories from the files in the config directory.
+
+=cut
+
+sub read {
+	my $self = shift;
+	my $hfile;
+	my $escape = $_pfm->config->{e};
+	# some defaults
+	@_command_history = ('du -ks * | sort -n', "man ${escape}1");
+	@_mode_history	 = ('755', '644');
+	@_path_history	 = ('/', $ENV{HOME});
+	@_regex_history	 = ('\.jpg$');
+#	@time_history;
+#	@perlcmd_history;
+	foreach (keys(%HISTORIES)) {
+		$hfile = $_pfm->config->CONFIGDIRNAME . "/$_";
+		if (-s $hfile and open (HISTFILE, $hfile)) {
+			chomp( @{$HISTORIES{$_}} = <HISTFILE> );
+			close HISTFILE;
+		}
+	}
+}
+
+=item write()
+
+Writes the histories to files in the config directory.
+
+=cut
+
+sub write {
+	my $self = shift;
+	my $failed;
+	my $screen = $_pfm->screen;
+	$screen->at(0,0)->clreol();
+	foreach (keys(%HISTORIES)) {
+		if (open HISTFILE, '>'.$_pfm->config->CONFIGDIRNAME."/$_") {
+			print HISTFILE join "\n", @{$HISTORIES{$_}}, '';
+			close HISTFILE;
+		} elsif (!$failed) {
+			$screen->putmessage("Unable to save (part of) history: $!");
+			$failed++; # warn only once
+		}
+	}
+	$screen->putmessage('History written successfully') unless $failed;
+	$screen->error_delay();
+	$screen->important_delay() if $failed;
+	$screen->set_deferred_refresh($screen->R_MENU);
+}
+
+=item write_dirs()
+
+Writes the current directory and swap directory to files in
+the config directory.
+
+=cut
+
+sub write_dirs {
+	my $self = shift;
+	my $configdirname = $_pfm->config->CONFIGDIRNAME;
+	my $swap_state	  = $_pfm->state(1);
+	
+	if (open CWDFILE, ">$configdirname/$CWDFILENAME") {
+		print CWDFILE $_pfm->state->currentdir, "\n";
+		close CWDFILE;
+	} else {
+		$_pfm->screen->putmessage(
+			"Unable to create $configdirname/$CWDFILENAME: $!\n"
+		);
+	}
+	if (defined($swap_state) && $_pfm->config->{swap_persistent} &&
+		open SWDFILE,">$configdirname/$SWDFILENAME")
+	{
+		print SWDFILE $swap_state->currentdir, "\n";
+		close SWDFILE;
+	} else {
+		unlink "$configdirname/$SWDFILENAME";
+	}
+}
+
+=item input()
+
+Prompts for input from the keyboard; pushes this input onto
+the appropriate history.
+
+=cut
+
+sub input { # $history, $prompt, [$default_input]
+	local $SIG{INT} = 'IGNORE'; # do not interrupt pfm
+	my ($self, $history, $prompt, $input) = @_;
+	$history = $HISTORIES{$history};
+	$prompt ||= '';
+	$input  ||= '';
+	$self->_set_term_history(@$history);
+	$input = $_keyboard->readline($prompt, $input);
+	if ($input =~ /\S/ and $input ne ${$history}[-1]) {
+		push (@$history, $input);
+		shift (@$history) if ($#$history > $MAXHISTSIZE);
+	}
+	return $input;
+}
+
+=item setornaments()
+
+Determines from the config file settings which ornaments (bold, italic,
+underline) should be used for the command prompt, then instructs
+Term::ReadLine to use these.
+
+=cut
+
+sub setornaments {
+	my ($self, $color) = @_;
+	my @cols;
+	$color ||=
+		$_pfm->config->{framecolors}{$_pfm->screen->color_mode}{message};
+	unless (exists $ENV{PERL_RL}) {
+		# this would have been nice, however,
+		# readline processes only the first (=most important) capability
+		push @cols, 'mr' if ($color =~ /reverse/);
+		push @cols, 'md' if ($color =~ /bold/);
+		push @cols, 'us' if ($color =~ /under(line|score)/);
+#		$kbd->ornaments(join(';', @cols) . ',me,,');
+		$_keyboard->ornaments($cols[0] . ',me,,');
+	}
+}
+
+##########################################################################
+
+=back
+
+=head1 CONSTANTS
+
+This package provides the B<H_*> constants which indicate the different
+types of input histories. They are:
+
+=over
+
+=item H_COMMAND
+
+The history of shell commands entered, I<e.g.> for the B<O> command.
+
+=item H_MODE
+
+The history of file modes (permission bits).
+
+=item H_PATH
+
+The history of file- and directory paths entered, I<e.g.> entered for
+the B<M>ore - B<S>how command.
+
+=item H_REGEX
+
+The history of regular expressions entered.
+
+=item H_TIME
+
+The history of times entered, I<e.g.> for the B<T> command.
+
+=item H_PERLCMD
+
+The history of Perl commands entered for the B<@> command.
+
+=back
+
+An input line may be stored in one of the histories by providing
+one of these constants to input() I<e.g.>
+
+    $self->input(H_PATH);
+
+=head1 SEE ALSO
+
+pfm(1).
+
+=cut
+
+1;
+
+# vim: set tabstop=4 shiftwidth=4:
---
+++ trunk/lib/App/PFM/State.pm
@@ -0,0 +1,147 @@
+#!/usr/bin/env perl
+#
+##########################################################################
+# @(#) PFM::State 0.12
+#
+# Name:			PFM::State.pm
+# Version:		0.12
+# Author:		Rene Uittenbogaard
+# Created:		1999-03-14
+# Date:			2010-04-10
+#
+
+##########################################################################
+
+=pod
+
+=head1 NAME
+
+PFM::State
+
+=head1 DESCRIPTION
+
+PFM class used for storing the current state of the application.
+
+=head1 METHODS
+
+=over
+
+=cut
+
+##########################################################################
+# declarations
+
+package PFM::State;
+
+use base 'PFM::Abstract';
+
+use PFM::Directory;
+
+use strict;
+
+my $_pfm;
+
+##########################################################################
+# private subs
+
+=item _init()
+
+Initializes new instances. Called from the constructor.
+Instantiates a PFM::Directory object.
+
+=cut
+
+sub _init {
+	my ($self, $pfm) = @_;
+	$_pfm					= $pfm;
+	$self->{_directory}		= new PFM::Directory($pfm);
+	# We might not have useful values for these yet since the config file
+	# might not have been read yet.
+	$self->{_position}		= undef;
+	$self->{_baseindex}		= undef;
+	$self->{multiple_mode}	= 0;
+	$self->{dot_mode}		= undef;
+	$self->{radix_mode}		= undef;
+	$self->{sort_mode}		= undef;
+	$self->{white_mode}		= undef;
+	# path_mode    sits in PFM::Directory
+	# color_mode   sits in PFM::Screen
+	# ident_mode   sits in PFM::Screen::Diskinfo
+	# mouse_mode   sits in PFM::Browser
+	# swap_mode    sits in PFM::Browser
+	# clobber_mode sits in PFM::CommandHandler
+}
+
+=item _clone()
+
+Performs one phase of the cloning process by cloning an existing
+PFM::Directory instance.
+
+=cut
+
+sub _clone {
+	my ($self, $original, @args) = @_;
+	$self->{_directory} = $original->{_directory}->clone(@args);
+}
+
+##########################################################################
+# constructor, getters and setters
+
+=item directory()
+
+Getter/setter for the PFM::Directory object.
+
+=cut
+
+sub directory {
+	my ($self, $value) = @_;
+	$self->{_directory} = $value if defined $value;
+	return $self->{_directory};
+}
+
+=item currentdir()
+
+Getter/setter for the current directory path.
+If a new directory is provided, it will be passed to PFM::Directory.
+
+=cut
+
+sub currentdir {
+	my $self = shift;
+	return $self->{_directory}->path(@_);
+}
+
+##########################################################################
+# public subs
+
+=item prepare()
+
+Prepares the contents of this state object. Called in case this state
+is not to be displayed on-screen right away.
+
+=cut
+
+sub prepare {
+	my ($self, $path) = @_;
+	$self->{dot_mode}	= $_pfm->config->{dot_mode};
+	$self->{radix_mode}	= $_pfm->config->{radix_mode};
+	$self->{sort_mode}	= $_pfm->config->{sort_mode};
+	$self->{white_mode}	= $_pfm->config->{white_mode};
+	$self->{_position}	= '.';
+	$self->{_baseindex}	= 0;
+	$self->{_directory}->prepare($path);
+}
+
+##########################################################################
+
+=back
+
+=head1 SEE ALSO
+
+pfm(1).
+
+=cut
+
+1;
+
+# vim: set tabstop=4 shiftwidth=4:
